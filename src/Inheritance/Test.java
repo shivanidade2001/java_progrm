322a670db34a377928924e00383ec55a3422559d
shivani <shivanidade2001@gmail.com>
2022-10-14 20:05:12 +0530

    nested class

--- src/Inheritance/Test.java
+++ src/Inheritance/Test.java
@@ -2,8 +2,6 @@ package Inheritance;
 
 public class Test {
     public static void main(String[] args) {
-
-
         A1 ab=new B1();
         System.out.println(ab.a);
         System.out.println(ab.b);
